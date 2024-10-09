0aca1523e3ff48b93d5976bc50d1c93847e4f4c0
Aidan Lynde <130251073+aidanlynde@users.noreply.github.com>
2024-10-09 11:06:51 -0500

    Add types directory
    included .ts file for the react-simple-maps

---
+++ types/react-simple-maps.d.ts
@@ -0,0 +1,10 @@
+// types/react-simple-maps.d.ts
+
+declare module 'react-simple-maps' {
+    export const ComposableMap: any;
+    export const Geographies: any;
+    export const Geography: any;
+    export const Marker: any;
+    // Add other exports if you use more components from the library
+  }
+  
