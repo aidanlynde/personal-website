db253f17ac79b082524bd91b1f787695663ba7e5
Aidan Lynde <130251073+aidanlynde@users.noreply.github.com>
2024-10-09 11:44:09 -0500

    Update type declaration module
    adds more components

--- types/react-simple-maps.d.ts
+++ types/react-simple-maps.d.ts
@@ -1,10 +1,44 @@
 // types/react-simple-maps.d.ts
 
 declare module 'react-simple-maps' {
-    export const ComposableMap: any;
-    export const Geographies: any;
-    export const Geography: any;
-    export const Marker: any;
+    import React from 'react';
+  
+    interface ComposableMapProps {
+      projection?: string;
+      projectionConfig?: object;
+      width?: number;
+      height?: number;
+      style?: React.CSSProperties;
+      children?: React.ReactNode;
+    }
+  
+    export const ComposableMap: React.FC<ComposableMapProps>;
+  
+    interface GeographiesProps {
+      geography: any;
+      children: (props: { geographies: any[]; projection: any }) => React.ReactNode;
+    }
+  
+    export const Geographies: React.FC<GeographiesProps>;
+  
+    interface GeographyProps {
+      geography: any;
+      fill?: string;
+      stroke?: string;
+      style?: React.CSSProperties;
+    }
+  
+    export const Geography: React.FC<GeographyProps>;
+  
+    interface MarkerProps {
+      coordinates: [number, number];
+      style?: React.CSSProperties;
+      children?: React.ReactNode;
+      onClick?: () => void;
+    }
+  
+    export const Marker: React.FC<MarkerProps>;
+  
     // Add other exports if you use more components from the library
   }
   
